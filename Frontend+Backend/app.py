62b19e2eab9d4849958c7ab6678c73f92d3fcc9a
Pusthakala Dharan Tej <57841685+dharantej1@users.noreply.github.com>
2022-11-23 07:08:24 +0000

    normalization of input values from the frontend

--- Frontend+Backend/app.py
+++ Frontend+Backend/app.py
@@ -6,6 +6,8 @@ import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import seaborn as sns
+# from sklearn.preprocessing import StandardScaler
+from joblib import load
 
 app = Flask(__name__,template_folder="./")
 
@@ -21,6 +23,10 @@ def hello_world():
 def predict():
     int_features=[float(x) for x in request.form.values()]
     data_to_be_predicted=np.array(int_features,ndmin=2)
+    scaler=load('std_scaler.bin')
+    print("data is ",data_to_be_predicted)
+    data_to_be_predicted = scaler.transform(data_to_be_predicted)
+    print("updated data is ",data_to_be_predicted)
     final_result=model.predict(data_to_be_predicted)
     if abs(final_result)>0.5:
         return render_template("waterqualityanalysis.html",pred="The Sample of Water is Suitable for Drinking")
