468275f5d29b9b5430011cfaa9d791276077873b
Pusthakala Dharan Tej <57841685+dharantej1@users.noreply.github.com>
2022-12-19 23:33:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,4 +154,4 @@ def download():
         return render_template("waterqualityanalysis.html",pred="*** Please Enter the Values and Click the Predict Button! ***")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
